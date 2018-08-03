8b66b73e9ca0f1cdc27e99d84d99296cc1345538
paubric <paubric@gmail.com>
2018-08-03 17:56:03 +0300

    Update ml.py with test

--- ml/ml.py
+++ ml/ml.py
@@ -1,7 +1,30 @@
 from sklearn import linear_model
 import pickle
+import numpy as np
+
+def train(X, Y):
+    # Create linear regression object
+    model = linear_model.LinearRegression()
+
+    # Train the model using the training sets
+    model.fit(X, Y)
+
+    #  Model persistence
+    s = pickle.dump(model, open( 'model.pk', 'wb' ))
 
 def predict(X):
-    clf = pickle.loads(s)
-    prediction = clf.predict(X)
-    return prediction
+    X = np.asarray(X)
+    X = np.reshape(X, (-1, 1))
+
+    model = pickle.load(open( 'model.pk', 'rb' ))
+
+    Y = model.predict(X)
+
+    return Y
+
+def test():
+    X = [1, 2, 3, 4]
+    Y = [2, 4, 6, 8]
+
+    X = np.reshape(X, (-1, 1))
+    train(X, Y)
