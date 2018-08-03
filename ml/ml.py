925a4b03a5f0ab7018e11791abc9aa866d6dae9f
paubric <paubric@gmail.com>
2018-08-03 18:10:25 +0300

    Update ml.py

--- ml/ml.py
+++ ml/ml.py
@@ -3,6 +3,10 @@ import pickle
 import numpy as np
 
 def train(X, Y):
+    # Input preprocessing
+    X = np.asarray(X)
+    X = np.reshape(X, (-1, 1))
+
     # Create linear regression object
     model = linear_model.LinearRegression()
 
@@ -10,21 +14,24 @@ def train(X, Y):
     model.fit(X, Y)
 
     #  Model persistence
-    s = pickle.dump(model, open( 'model.pk', 'wb' ))
+    pickle.dump(model, open( 'model.pk', 'wb' ))
 
 def predict(X):
+    # Input preprocessing
     X = np.asarray(X)
     X = np.reshape(X, (-1, 1))
 
+    # Load trained model from file
     model = pickle.load(open( 'model.pk', 'rb' ))
 
+    # Model inference
     Y = model.predict(X)
 
     return Y
 
 def test():
+    # Test function to generate pickle file with dummy regression model
     X = [1, 2, 3, 4]
     Y = [2, 4, 6, 8]
 
-    X = np.reshape(X, (-1, 1))
     train(X, Y)
